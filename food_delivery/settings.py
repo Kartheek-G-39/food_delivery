3ee7da9d2254508a575e1e113bc2dc7efb702a4b
codewriter@creative <107845549+Kartheek-G-39@users.noreply.github.com>
2024-08-20 03:42:21 +0530

    Update settings.py

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-jy*zf-f1pw^@q(!*qwh##$^srncs^=#m&6-t8eq_+zm@io-j1&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*',"https://fooddelivery.azurewebsites.net"]
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'core.CustomUser'
 # Application definition
